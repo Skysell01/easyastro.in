8950238077d3af0428a1db2186c70b4ff47bd51a
Skysell01 <Aarohi@skysellmedia.com>
2026-04-07 13:28:03 +0530

    added

--- src/app/cart-cashfree/page.tsx
+++ src/app/cart-cashfree/page.tsx
@@ -284,7 +284,7 @@ const createPaymentSession = async () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_CART_SUPABASE_ANON_KEY}`,
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_FUNCTION_SUPABASE_ANON_KEY}`,
       },
       body: JSON.stringify(payload),
     });
