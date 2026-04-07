d7aeb67b2ba63ccc5b7f69543be8fe2975e4de12
Skysell01 <Aarohi@skysellmedia.com>
2026-04-07 15:51:16 +0530

    added

--- src/app/order-confirmation-cashfree/OrderConfirmationClient.tsx
+++ src/app/order-confirmation-cashfree/OrderConfirmationClient.tsx
@@ -38,7 +38,10 @@ export default function OrderConfirmationClient({ orderId: paramOrderId }: { ord
 
       const res = await fetch(`${FUNCTIONS_URL}/verify-payment`, {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: { "Content-Type": "application/json" ,
+          "Authorization": `Bearer ${process.env.NEXT_PUBLIC_FUNCTION_SUPABASE_ANON_KEY}`,
+         },
+        
         body: JSON.stringify({ orderId }),
       });
 
