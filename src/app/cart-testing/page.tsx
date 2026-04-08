c89d52db36cfdd637a2fa7a5605a0d7bd99e293b
Skysell01 <Aarohi@skysellmedia.com>
2026-04-08 11:34:28 +0530

    added

--- src/app/cart-testing/page.tsx
+++ src/app/cart-testing/page.tsx
@@ -306,7 +306,7 @@ const createPaymentSession = async () => {
       phoneNumber: orderData.phone_number,
       additional_products: additionalProductsData,
       product_name: "Soulmate Sketch",
-      url: `${window.location.origin}/order-confirmation-testing`,
+      url: `${window.location.origin}/order-confirmation`,
     };
 
     // ✅ Save locally
