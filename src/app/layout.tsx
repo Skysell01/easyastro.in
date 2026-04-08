a31692d5914def5e4480fb4077c257bdd25438ee
Skysell01 <Aarohi@skysellmedia.com>
2026-04-08 17:37:55 +0530

    added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -233,7 +233,7 @@ fbq('track', 'PageView');
         </Script>
 
         {/* ================= noscript fallback ================= */}
-       <noscript><img height="1" width="1" style={{"display:none"}}
+       <noscript><img height="1" width="1" style={{display:"none"}}
 src="https://www.facebook.com/tr?id=2055279592036652&ev=PageView&noscript=1"
 /></noscript>
 
